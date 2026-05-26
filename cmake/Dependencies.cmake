5b9a858e4fb02f1564ca17b91c5da8371d0f5094
Robert King <varitonium@gmail.com>
2026-05-26 21:03:36 +0200

    OBN-37: upe-relocation of cache proj dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -61,6 +61,26 @@ endif()
 
 include(FetchContent)
 
+set(FETCHCONTENT_BASE_DIR
+    "${CMAKE_SOURCE_DIR}/.deps"
+    CACHE PATH "FetchContent cache"
+    FORCE
+)
+
+set(DOCTEST_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/doctest-src")
+
+if(EXISTS "${DOCTEST_SOURCE_DIR}/CMakeLists.txt")
+    message(STATUS "Using cached doctest: ${DOCTEST_SOURCE_DIR}")
+
+    set(FETCHCONTENT_SOURCE_DIR_DOCTEST
+        "${DOCTEST_SOURCE_DIR}"
+        CACHE PATH "Cached doctest source"
+        FORCE
+    )
+else()
+    message(STATUS "Fetching doctest")
+endif()
+
 FetchContent_Declare(
     doctest
     GIT_REPOSITORY https://github.com/doctest/doctest.git
