72a0dc31d68812ed2e6d428de832167fee845140
Robert King <varitonium@gmail.com>
2026-06-02 23:54:17 +0200

    OBN-40: dev-adding of tcxx-20 dependency

---
+++ cmake/Dependencies-repo.cmake
@@ -0,0 +1,50 @@
+
+# target_include_directories(xpic_processor PRIVATE
+#     ${tcxx_SOURCE_DIR}/include
+# )
+
+include(FetchContent)
+
+set(FETCHCONTENT_BASE_DIR
+    "${CMAKE_SOURCE_DIR}/.deps"
+    CACHE PATH "FetchContent cache"
+    FORCE
+)
+
+#tcxx-20 ========================================================
+
+FetchContent_Declare(
+    tcxx
+    GIT_REPOSITORY https://github.com/variton/tcxx.git
+    GIT_TAG main
+    GIT_SHALLOW TRUE
+
+    # Prevent tcxx's own CMakeLists.txt from being included
+    SOURCE_SUBDIR cmake-do-not-add
+)
+
+FetchContent_MakeAvailable(tcxx)
+
+#doctest =======================================================
+
+set(DOCTEST_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/doctest-src")
+
+if(EXISTS "${DOCTEST_SOURCE_DIR}/CMakeLists.txt")
+    message(STATUS "Using cached doctest: ${DOCTEST_SOURCE_DIR}")
+
+    set(FETCHCONTENT_SOURCE_DIR_DOCTEST
+        "${DOCTEST_SOURCE_DIR}"
+        CACHE PATH "Cached doctest source"
+        FORCE
+    )
+else()
+    message(STATUS "Fetching doctest")
+endif()
+
+FetchContent_Declare(
+    doctest
+    GIT_REPOSITORY https://github.com/doctest/doctest.git
+    GIT_TAG v2.4.12
+)
+
+FetchContent_MakeAvailable(doctest)
