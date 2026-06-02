5e627343eedb699d0051882f0db331d77615f0fa
Robert King <varitonium@gmail.com>
2026-06-02 23:54:17 +0200

    OBN-40: dev-adding of tcxx-20 dependency

--- cmake/Dependencies-pkg.cmake
+++ cmake/Dependencies-pkg.cmake
@@ -58,33 +58,3 @@ if(NOT GCOV_EXECUTABLE)
         "  sudo apt install gcov"
     )
 endif()
-
-include(FetchContent)
-
-set(FETCHCONTENT_BASE_DIR
-    "${CMAKE_SOURCE_DIR}/.deps"
-    CACHE PATH "FetchContent cache"
-    FORCE
-)
-
-set(DOCTEST_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/doctest-src")
-
-if(EXISTS "${DOCTEST_SOURCE_DIR}/CMakeLists.txt")
-    message(STATUS "Using cached doctest: ${DOCTEST_SOURCE_DIR}")
-
-    set(FETCHCONTENT_SOURCE_DIR_DOCTEST
-        "${DOCTEST_SOURCE_DIR}"
-        CACHE PATH "Cached doctest source"
-        FORCE
-    )
-else()
-    message(STATUS "Fetching doctest")
-endif()
-
-FetchContent_Declare(
-    doctest
-    GIT_REPOSITORY https://github.com/doctest/doctest.git
-    GIT_TAG v2.4.12
-)
-
-FetchContent_MakeAvailable(doctest)
