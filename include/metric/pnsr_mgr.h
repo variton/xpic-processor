76bfce3305fe3861c3423bf9802b8700aa7710d5
Robert King <varitonium@gmail.com>
2026-06-27 23:07:46 +0200

    OBN-43: fix-valgrind pnsr test

--- include/metric/pnsr_mgr.h
+++ include/metric/pnsr_mgr.h
@@ -2,9 +2,9 @@
 #ifndef PSNR_MGR_H
 #define PSNR_MGR_H
 
+#include <cstdint>
 #include <ncnm.h>
 #include <span>
-#include <cstdint>
 
 #include <error_info.h>
 #include <errty.h>
@@ -12,34 +12,32 @@
 
 namespace metric {
 
-template <typename T>
-using	Default = core::NCNM<T>;
+template <typename T> using Default = core::NCNM<T>;
 
 enum class PSNRMgrError {
   SSENullError,
 };
 
-/**
- * @brief Structured error information for @ref FileHandler operations.
- */
 using PSNRMgrErrorInfo = err::ErrorInfo<PSNRMgrError>;
 
 static_assert(topology::ErrorInfoTy<PSNRMgrErrorInfo>);
 
-class PSNRMgr: public Default<PSNRMgr>{
+class PSNRMgr : public Default<PSNRMgr> {
 
 public:
-  explicit PSNRMgr(std::span<const std::uint8_t> original, 
+  explicit PSNRMgr(std::span<const std::uint8_t> original,
                    std::span<const std::uint8_t> watermarked) noexcept;
   ~PSNRMgr();
 
-  tl::expected<double,PSNRMgrErrorInfo> computePNSR(int width,int height,int channels) noexcept;
+  tl::expected<double, PSNRMgrErrorInfo> computePNSR(int width, 
+                                                     int height,
+                                                     int channels) noexcept;
 
 private:
   std::span<const std::uint8_t> original_;
   std::span<const std::uint8_t> watermarked_;
 };
 
-}
+} // namespace metric
 
 #endif
--- src/metric/test/test_pnsr_mgr.cpp
+++ src/metric/test/test_pnsr_mgr.cpp
@@ -4,35 +4,37 @@
 
 #include <string>
 
+#include <env_mgr.h>
 #include <filehandler.h>
+#include <fmt/core.h>
 #include <inputimg.h>
 #include <pnsr_mgr.h>
-#include <env_mgr.h>
-
-// #include <filehandler.h>
-// #include <inputimg.h>
-// #include <jpeg_compressor.h>
 #include <jpeg_decompressor.h>
 
 TEST_CASE("ComputePnsr OK") {
-  auto &local = platform::EnvMgr::get_instance();
-  auto rc = local.get_env_var("RC");
+  // auto &local = platform::EnvMgr::get_instance();
+  // auto rc = local.get_env_var("RC");
+  //temporary patch
+  std::string rc{"/home/cxx-core/rc"};
   using namespace img;
-  std::string input{rc.value()+"/ur.jpg"};
+  std::string input{rc + "/ur.jpg"};
   auto input_file = fio::FileHandler::open(input.data(), "rb");
   CHECK(input_file.has_value());
 
   JpegDecompressor decompressor{};
   auto ret_init_decompressor = decompressor.init(input_file.value().get());
   CHECK(ret_init_decompressor.has_value());
-  auto ret_decompress = decompressor.decompress();
+  // auto ret_decompress = decompressor.decompress();
   CHECK(ret_decompress.has_value());
   // image object creation ==================================================
   InputImg inputimg{decompressor.cinfo()};
 
-  // pnsr mgr ===============================================================
-  //auto pnsr_mgr = metric::PSNRMgr{};
-  //auto pnsr = pnsr_mgr.computePNSR(inputimg.width, inputimg.height, inputimg.components);
+  pnsr mgr ===============================================================
+  auto pnsr_mgr = metric::PSNRMgr{};
+  auto pnsr = pnsr_mgr.computePNSR(inputimg.width, inputimg.height,
+  inputimg.components); auto ret_finish_decompression =
+  decompressor.finish_decompress();
+  CHECK(ret_finish_decompression.has_value());
 }
 
 // todo fix test coverage
