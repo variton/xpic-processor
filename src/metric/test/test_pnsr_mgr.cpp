b51b29f3470c2e51572c017be11a2cb807f261c7
Robert King <varitonium@gmail.com>
2026-06-28 23:26:16 +0200

    OBN-45: dev-jpeg decoder impl

--- src/metric/test/test_pnsr_mgr.cpp
+++ src/metric/test/test_pnsr_mgr.cpp
@@ -8,19 +8,21 @@
 #include <filehandler.h>
 #include <fmt/core.h>
 #include <inputimg.h>
-#include <pnsr_mgr.h>
+#include <jpeg_decoder.h>
 #include <jpeg_decompressor.h>
+#include <pnsr_mgr.h>
 
 TEST_CASE("ComputePnsr OK") {
   // auto &local = platform::EnvMgr::get_instance();
   // auto rc = local.get_env_var("RC");
-  //temporary patch
+  // temporary patch
   std::string rc{"/home/cxx-core/rc"};
   using namespace img;
   std::string input{rc + "/ur.jpg"};
   auto input_file = fio::FileHandler::open(input.data(), "rb");
   CHECK(input_file.has_value());
-
+  // Jpeg decompressor creation
+  // ==================================================
   JpegDecompressor decompressor{};
   auto ret_init_decompressor = decompressor.init(input_file.value().get());
   CHECK(ret_init_decompressor.has_value());
@@ -28,9 +30,19 @@ TEST_CASE("ComputePnsr OK") {
   CHECK(ret_decompress.has_value());
   // image object creation ==================================================
   InputImg inputimg{decompressor.cinfo()};
-  //pnsr mgr ===============================================================
-  // auto pnsr_mgr = metric::PSNRMgr{};
-  // auto pnsr = pnsr_mgr.computePNSR(inputimg.width, inputimg.height,inputimg.components); 
+  // jpeg decoder ===========================================================
+  /*the decoding is a bottleneck 2x time*/
+  JpegDecoder original{inputimg.width, inputimg.height, inputimg.components};
+  original.decode(decompressor);
+  JpegDecoder watermarked{inputimg.width, inputimg.height, inputimg.components};
+  watermarked.decode(decompressor);
+  // pnsr mgr ===============================================================
+  metric::PSNRMgr pnsr_mgr{original.pixels(), watermarked.pixels()};
+  auto ret_pnsr = pnsr_mgr.computePNSR(inputimg.width, inputimg.height,
+                                       inputimg.components);
+  CHECK(ret_pnsr.has_value());
+  volatile double pnsr = ret_pnsr.value();
+  CHECK(ret_pnsr.value() > 0);
 }
 
 // todo fix test coverage
