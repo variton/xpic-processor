f72e6188dc71247e15551bd62137b6685bde29b8
Robert King <varitonium@gmail.com>
2026-07-17 23:46:34 +0200

    OBN-65: upe-clock runtime macro impl

--- benchmark/sequence.cpp
+++ benchmark/sequence.cpp
@@ -4,18 +4,31 @@
 #include <thread>
 #include <fmt/core.h>
 
-//[[expected::runtime]]
-int main(int argc, char **argv) {
-  if (argc == 1) {
-    fmt::println("sequence missing argument");
-    return EXIT_FAILURE;
-  }
+//todo: should be moved to a tcxx23 and tcxx
+//define a runtime for all sorts of clocks
+#define MICRO_CLOCK_RUNTIME_MAIN                                      \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, obs::micro_c& clock);                    \
+                                                                         \
+    int main(int argc, char** argv) {                                   \
+        if (argc == 1) {                                                \
+            fmt::println("sequence missing argument");                  \
+            return EXIT_FAILURE;                                        \
+        }                                                               \
+                                                                         \
+        obs::micro_c clock;                                             \
+        return clock_runtime_main(argc, argv, clock);                    \
+    }                                                                   \
+                                                                         \
+    static int clock_runtime_main(                                      \
+        int argc, char** argv, [[maybe_unused]] obs::micro_c& clock)
 
-  using namespace std::chrono_literals;
-  obs::micro_c clock;
+
+MICRO_CLOCK_RUNTIME_MAIN {
+  //using namespace std::chrono_literals;
   clock();
   //quality check to gather picture metric 
-  std::this_thread::sleep_for(500ms);
+  //std::this_thread::sleep_for(500ms);
   clock();
   fmt::println("time elasped: {}", clock.elapsed());
   return 0;
